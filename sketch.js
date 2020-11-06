d6aabd45b0d829092055e60e33f1d0a14b832fc3
xsophiexu <58953096+xsophiexu@users.noreply.github.com>
2020-11-06 00:49:50 -0500

    image

--- sketch.js
+++ sketch.js
@@ -1,32 +1,80 @@
-let button1;
-let bg = 100;
-let img;
+let slider;
+let bg = 200;
+let x = 100;
+let y = 100;
+let w = 150;
+let h = 150;
+let sel;
+let ap;
+let ba;
+let or;
+let wa;
+let save = [];
 
-function setup() {
-  createCanvas(400, 400);
-    
-  img = loadImage('/images/apple.png');
-  button1 = createButton('My Button');
-  button1.position(300, 100);
-    //absolute position
-  button1.style('backgroundColor', '#FF0000');
-  button1.style('width', '80px');
-  button1.style('height', '40px');
-  button1.style('borderRadius', '4px');
-    //width, height, borderRadius, fontSize, fontWeight - bold
-    //rgba for transparency (0,0,0,0) for invisible
-  button1.mousePressed(bgChange);
-    //mouseOver for hovering
-  createColorPicker();
+function preload() {
+  ap = loadImage('apple.png');
+  ba = loadImage('banana.png');
+  or = loadImage('orange.png');
+  wa = loadImage('watermelon.png');
+}
+
+function setup(){
+  createCanvas(windowWidth, windowHeight);
+  //save.push(bg);
+  background(bg);
+  slider = createSlider(0, 255, 150);
+  slider.position(width/2 - slider.width/2, height/2 - slider.height/2);
+  textAlign(CENTER);
+  sel = createSelect();
+  sel.position(10, 10);
+  sel.option('apple');
+  sel.option('banana');
+  sel.option('orange');
+  sel.option('watermelon');
+  sel.changed(printFruit);
 }
 
 function draw() {
-    background(bg);
-    image(img, 100, 100);
-    //rect(200, 200, 100, 100);
-    //button1.position(windowWidth/6, 100);
+  background(bg);
+  //printFruit();
+  for (let obj of save){
+      for(let i = 0; i<10; i++){
+          image(ap, save[i].x, save[i].y, save[i].w, save[i].h);
+      }
+  }
+  bg = slider.value();
+}
+
+function printFruit(){
+    //background(bg);
+    let item = sel.value();
+    if(item == 'apple'){
+        save.push({
+            f: ap;
+            x: x;
+            y: y;
+            w: w;
+            h: h;
+        });
+        
+    } else if (item == 'banana'){
+        image(ba, x, y, w, h);
+    } else if (item == 'orange'){
+        image(or, x, y, w, h);
+    } else if (item == 'watermelon'){
+        image(wa, x, y, w, h);
+    }
 }
 
 function bgChange(){
     bg = color(random(255), random(255), random(255));
+    //background(save[0]);
+    //image(ap, x, y, w, h);
+}
+
+
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+    //background(save[0]);
+    slider.position(width/2 - slider.width/2, height/2 - slider.height/2);
 }
